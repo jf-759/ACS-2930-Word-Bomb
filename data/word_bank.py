80e2212d88df432391192313d9debefd77cec7ec
Xica Felix <xicasfelix@gmail.com>
2025-02-03 15:25:46 -0500

    create word bank class and data directory

---
+++ data/word_bank.py
@@ -0,0 +1,27 @@
+
+import random
+
+class WordBank:
+  def __init__(self):
+    # TODO Change the clusters depending on the difficulty
+    self.letter_clusters = [
+    "st", "tr", "ch", "ph", "sh", "th", "gr", "cr", "dr", "fr",
+    "bl", "br", "cl", "pl", "pr", "qu", "sc", "sl", "sp", "sw",
+    "wr", "wh", "sn", "fl", "sk"
+    ]
+    self.used_cluster = []
+    self.current_cluster = ''
+
+  '''
+    This function gets a letter cluster for the user to create words with
+  '''
+  def get_cluster(self):
+    if self.letter_clusters:
+      for word in range(len(self.letter_clusters)):
+        self.current_cluster = random.choice(self.letter_clusters)
+        self.used_cluster.append(self.current_cluster)
+        return self.current_cluster
+      print('No letter clusters remaining')
+      return False
+    
+    # TODO Implement word validation for user input: is this a real word, does it contain the letter cluster
